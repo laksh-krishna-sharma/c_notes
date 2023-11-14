72aa4cc8520c527e74328ffa80356cb4c82de2a3
laksh-krishna-sharma <asklakshsharma@gmail.com>
2023-11-14 21:23:58 +0530

    lecture 2

--- C/lecture2/recursion.c
+++ C/lecture2/recursion.c
@@ -1,13 +1,27 @@
 // RECURSION in simple words is a function calling itself
 #include <stdio.h>
 
-void great(int n){
+// void great(int n){
+//     if (n==0) return ;
+//     printf("Good Morning\n");
+//     great(n-1);
+// }
+// int main (){
+//     great(3);
+
+//     return 0;
+// }
+
+void prt(int n){
     if (n==0) return ;
-    printf("Good Morning\n");
-    great(n-1);
+    printf("%d\n",n);
+    prt(n-1);
 }
-int main (){
-    great(3);
+int main(){
+    printf("Enter a number : ");
+    int num;
+    scanf("%d",&num);
+    prt(num);
 
     return 0;
 }
