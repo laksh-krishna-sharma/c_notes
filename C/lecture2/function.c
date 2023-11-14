9c5f7d8cc6d063486d31bfc158dfb8cc09ffef2d
laksh-krishna-sharma <asklakshsharma@gmail.com>
2023-11-14 17:08:53 +0530

    lecture 2

--- C/lecture2/function.c
+++ C/lecture2/function.c
@@ -33,6 +33,18 @@ void oddeven(int num){ //here num is known as formal argument
 // Scope for a variable inside a function 
 // ek function ke andar define hua wa variable (local variable) ko b us function ke andar he acess kr sak te hai us ke bhar it is not posible to acess that variable
 
+// Function Prototype 
+// agar function 1 ke andar function 2 ko call kra hai to its nesecarry ki function 2 function 1 ke uppar likha hua ho nahi to error ayaga
+// isa solve kr ne ke liya function prototype ko use kr sak te hai
+
+// int fn1(){
+//     void fn2();    // this is function prototype
+//     fn2();
+// }
+// void fn2(){
+
+// }
+
 int main(){
     
     // for (int i = 0; i < 10 ; i++)
