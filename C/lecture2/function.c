91e002c449fc3a98b11e3ac5dd1b508e3e201294
laksh-krishna-sharma <asklakshsharma@gmail.com>
2023-11-14 16:45:44 +0530

    lecture 2

--- C/lecture2/function.c
+++ C/lecture2/function.c
@@ -14,10 +14,36 @@ void hello(){
     printf("hello\n");
 }
 
-int main(){
-    for (int i = 0; i < 10 ; i++)
+// function with parameter 
+// agar actual argument mai koi value pass nahi hui hai aur hum ne formal argument mai koi default value pass kr rakhi hai (num =2) to function mai formal argument ki default value pass ho ja ayagi
+ 
+void oddeven(int num){ //here num is known as formal argument
+    if (num%2==0)
     {
-        hello();
+        printf("Even number");
     }
+    else
+    {
+        printf("Odd number");
+    }
+    
+    return;    
+}
+
+// Scope for a variable inside a function 
+// ek function ke andar define hua wa variable (local variable) ko b us function ke andar he acess kr sak te hai us ke bhar it is not posible to acess that variable
+
+int main(){
+    
+    // for (int i = 0; i < 10 ; i++)
+    // {
+    //     hello();
+    // }
+
+    int num ;
+    printf("enter a number : ");
+    scanf("%i",&num);
+    oddeven(num);  //here num is known as actual argument
+    
     return 0;
 }
