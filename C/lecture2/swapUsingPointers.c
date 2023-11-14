b37ced98a4a00e69905356cc7d482515f774019f
laksh-krishna-sharma <asklakshsharma@gmail.com>
2023-11-14 20:22:13 +0530

    lecture 2

--- C/lecture2/swapUsingPointers.c
+++ C/lecture2/swapUsingPointers.c
@@ -1,13 +1,10 @@
-// PASS BY VALUE
+// PASS BY REFERENCE
 #include <stdio.h>
 
-void swap(int x , int y){
-    
-    x = x+y;
-    y = x-y;
-    x = x-y;
-    printf("number are swaped %d,%d",x,y);
-
+void swap(int* x , int* y){
+    int z = *x;
+    *x = *y;
+    *y = z;
     return ;
 }
 
@@ -21,7 +18,8 @@ int main(){
     printf("enter second number : ");
     scanf("%d",&b);
 
-    swap(a,b);
+    swap(&a,&b);
+    printf("number are swaped %d,%d",a,b);
 
     return 0;
 }
