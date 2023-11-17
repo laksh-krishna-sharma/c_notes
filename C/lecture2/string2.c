20cabff456de6d3617202ec51d9f105bfcbc8893
laksh-krishna-sharma <asklakshsharma@gmail.com>
2023-11-18 03:35:48 +0530

    lecture 2

---
+++ C/lecture2/string2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    char ch [50];
+    // thrie is a problem with scanf agar mai input mai 'hi i am laksh' du to wo sirf hi input le ga aur wahi print kre ga hi ke baad jo space ayaga waha se kuch input nahi ho ga and this problem can be solved by gets(for input) and puts(for output)
+    gets(ch);
+    puts(ch);
+
+    return 0;
+}
