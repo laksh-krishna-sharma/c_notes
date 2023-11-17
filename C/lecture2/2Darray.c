987cf77854d307d56d3f7bb0ed3c9b803464d43e
laksh-krishna-sharma <asklakshsharma@gmail.com>
2023-11-17 18:47:26 +0530

    lecture 2

--- C/lecture2/2Darray.c
+++ C/lecture2/2Darray.c
@@ -3,5 +3,17 @@
 int main()
 {
     // decleration
-    
+    int arr[3][2] = {{1,2},{3,4},{5,6}};
+    int arr2[3][2] = {1,2,3,4,5,6};
+    for( int i = 0;i < 3; i++)
+    {
+        for(int j = 0; j<2; j++)
+        {
+            printf("%d ",arr2[i][j]);
+        }
+        printf("\n");
+    }
+    int arr3[][2] = {1,2,3,4,5,6};
+
+    return 0;
 }
